af207cd72ed017cac3b7b67de9b73e52bb08bb1d
sarraayari <96915255+sarraayari@users.noreply.github.com>
2023-01-17 17:23:39 +0100

    cors

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = 'b\x85\xc9\x99\xc3\xb1\x81\x86\x96\xf3t\x91\xbb\rQ\xc
 app.config["MONGO_URI"]="mongodb+srv://sarra:1234@cluster0.p6dxnn8.mongodb.net/?retryWrites=true&w=majority"
 app.config['CONTENT_TYPE']='Content-Type'
 app.config['CORS_SUPPORTS_CREDENTIALS']= True
-app.config['CORS_RESOURCES']= {r"/manifest.json": {"origins":"https://*.netlify.app" }}
+app.config['CORS_RESOURCES']= {r"/manifest.json": {"origins":"*" }}
 
 cors=CORS(app)
 
