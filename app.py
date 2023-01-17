263ca5f04e2a77914c49e208c463d41267ef14ea
sarraayari <96915255+sarraayari@users.noreply.github.com>
2023-01-17 16:32:00 +0100

    CORS

--- app.py
+++ app.py
@@ -9,12 +9,18 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# {{"origins" : "https://icsa2023.netlify.app/*"}})
+# 
 app.config['UPLOAD_DIRECTORY'] = 'C:\\Users\\sarra\\Junior-Project\\server\\uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['SECRET_KEY'] = 'b\x85\xc9\x99\xc3\xb1\x81\x86\x96\xf3t\x91\xbb\rQ\xce\x18$\xd5\xa8\x10w$sR'
 app.config["MONGO_URI"]="mongodb+srv://sarra:1234@cluster0.p6dxnn8.mongodb.net/?retryWrites=true&w=majority"
-CORS(app,origins={"https://icsa2023.netlify.app/*"})
+app.config['CONTENT_TYPE']='Content-Type'
+app.config['CORS_SUPORTS_CREDENTIALS']= True
+CORS(app, resources={
+    r"/*":{
+        "origins":"*"
+    }
+})
 app.config['CORS_HEADERS']= 'Content_Type'
 
 client = MongoClient("mongodb+srv://sarra:1234@cluster0.p6dxnn8.mongodb.net/?retryWrites=true&w=majority")
