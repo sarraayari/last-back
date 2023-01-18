946669c8113f0770dcf3c7a71ab4e220b7f81790
sarraayari <96915255+sarraayari@users.noreply.github.com>
2023-01-19 00:15:40 +0100

    last-back url

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from werkzeug.exceptions import RequestEntityTooLarge
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_DIRECTORY'] = 'C:\\Users\\sarra\\Junior-Project\\server\\public\\uploads\\'
+app.config['UPLOAD_DIRECTORY'] = 'https://last-back-here.onrender.com/public/uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['SECRET_KEY'] = 'b\x85\xc9\x99\xc3\xb1\x81\x86\x96\xf3t\x91\xbb\rQ\xce\x18$\xd5\xa8\x10w$sR'
 app.config["MONGO_URI"]="mongodb+srv://sarra:1234@cluster0.p6dxnn8.mongodb.net/?retryWrites=true&w=majority"
@@ -82,11 +82,6 @@ def Upload():
 @cross_origin(origin='https://icsa2023.netlify.app/TTable', allow_headers=['Content-Type', 'Authorization'])
 def download_file(filename):
     filename='saeeaajjjjjjjjjjjjjjjjj.pdf'
-    binary_pdf = send_file(app.config['UPLOAD_DIRECTORY']+filename,as_attachment=True)
-    res = make_response(binary_pdf)
-    res.headers['Content-Type'] = 'application/pdf'
-    res.headers['Content-Disposition'] = \
-                'yourfilename'
     return (send_file(app.config['UPLOAD_DIRECTORY']+filename,as_attachment=True))############
 
 if __name__ == "__main__":
