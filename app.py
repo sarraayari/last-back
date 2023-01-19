080b57bf67162088ab5beacfdc4d98a3168cf656
sarraayari <96915255+sarraayari@users.noreply.github.com>
2023-01-19 12:45:05 +0100

    upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 #app.config['UPLOAD_DIRECTORY'] = 'C:\\Users\\sarra\\Junior-Project\\server\\public\\uploads\\'
-app.config['UPLOAD_DIRECTORY'] = 'https://last-back-here.onrender.com/public/uploads'
+app.config['UPLOAD_DIRECTORY'] = 'https://last-back-here.onrender.com/public/uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['SECRET_KEY'] = 'b\x85\xc9\x99\xc3\xb1\x81\x86\x96\xf3t\x91\xbb\rQ\xce\x18$\xd5\xa8\x10w$sR'
 app.config["MONGO_URI"]="mongodb+srv://sarra:1234@cluster0.p6dxnn8.mongodb.net/?retryWrites=true&w=majority"
@@ -47,7 +47,7 @@ def upload():
             if file:
                 filename=secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_DIRECTORY'],filename))
-                return ('file uploaded') 
+                return 'file uploaded'
 
 @app.route('/Upload', methods=['POST', 'GET'])
 # @cross_origin(origins=['https://last-front.netlify.app/AbstractSubmission','https://last-front.netlify.app/TTable'], allow_headers=['Content-Type', 'Authorization'])
