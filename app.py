c6b441d18f944ca3d71249c429e4f17a6c637182
sarraayari <96915255+sarraayari@users.noreply.github.com>
2023-01-19 17:46:49 +0100

    upload direct access/

--- app.py
+++ app.py
@@ -19,14 +19,14 @@ app.config['CONTENT_TYPE']='Content-Type'
 
 #     response.headers['Access-Control-Expose-Headers'] = 'Content-Length'
 
-#CORS(app)
+CORS(app)
 #cors=CORS(app ,resources={r"*": {"origins": 'https://last-front.netlify.app./*'}},supports_credentials=True)
 #cors = CORS(app, resources={r"/*": {"origins": "*"}})
 client = MongoClient("mongodb+srv://sarra:1234@cluster0.p6dxnn8.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database('Uploads')
 
 @app.route('/')
-@cross_origin(origin='*', allow_headers=['Content-Type', 'Authorization'])
+#@cross_origin(origin='*', allow_headers=['Content-Type', 'Authorization'])
 def entry_point():
     return ('home.html')#######
 
@@ -43,7 +43,7 @@ def handle_file_size_exceeded(error):
     return jsonify({"error": "File size exceeded maximum limit of 5MB"}), 400
     #############################
 @app.route('/upload', methods=['POST'])
-@cross_origin(origins='https://last-front.netlify.app/AbstractSubmission', allow_headers=['Content-Type', 'Authorization'])
+#@cross_origin(origins='https://last-front.netlify.app/AbstractSubmission', allow_headers=['Content-Type', 'Authorization'])
 def upload():
     if request.method == 'POST':
         if (request.files):
@@ -57,7 +57,7 @@ def upload():
 
 
 @app.route('/Upload', methods=['POST', 'GET'])
-@cross_origin(origins=['https://last-front.netlify.app/AbstractSubmission','https://last-front.netlify.app/TTable'], allow_headers=['Content-Type', 'Authorization'])
+#@cross_origin(origins=['https://last-front.netlify.app/AbstractSubmission','https://last-front.netlify.app/TTable'], allow_headers=['Content-Type', 'Authorization'])
 def Upload():
     if request.method == 'POST':
         FirstName=request.get_json()['FirstName']
@@ -88,7 +88,7 @@ def Upload():
 
 
 @app.route('/download/<path:filename>',methods=['GET'])
-@cross_origin(origin='https://last-front.netlify.app/TTable',methods=['GET'],allow_headers=['Content-Type', 'Authorization'])
+#@cross_origin(origin='https://last-front.netlify.app/TTable',methods=['GET'],allow_headers=['Content-Type', 'Authorization'])
 def download_file(filename):
     filename='saeeaajjjjjjjjjjjjjjjjj.pdf'
     #binairy=send_file(app.config['UPLOAD_DIRECTORY']+'/'+filename, as_attachment=True)
