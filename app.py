0e72f45d5bcb1f07b79b244f4b06871d0b6989f6
sarraayari <96915255+sarraayari@users.noreply.github.com>
2023-01-20 13:22:59 +0100

    upload direct access/

--- app.py
+++ app.py
@@ -89,14 +89,13 @@ def Upload():
         return dataJson
 
 
-@app.route('/download',methods=['GET'])  #/<path:filename>
+@app.route('/download')  #/<path:filename>  ,methods=['GET']
 #@cross_origin(origin='https://last-front.netlify.app/TTable',methods=['GET'],allow_headers=['Content-Type', 'Authorization'])
 def download_file(filename):
  
+    str='https://last-back-here.onrender.com/download/'+filename
 
-
-
- return 'houhou'
+    return str
 
 
 
